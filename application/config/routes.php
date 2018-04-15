e56c492e07ecc931f4e8478b4b5602c35ae8fdbe
Nikhil Verma <vrmanikhil2.0@gmail.com>
2018-04-15 18:02:36 +0530

    folder

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,7 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'home';
-$route['404_override'] = '';
+$route['404_override'] = 'home/pageNotFound';
 
 $route['dextryx'] = 'home/dextryx';
 $route['about-us'] = 'home/aboutUs';
